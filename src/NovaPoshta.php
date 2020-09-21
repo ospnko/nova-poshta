e9f256fb974faf45bc3317459f2729bf0163c8df
scary-layer <artur.osipenko@hotmail.com>
2020-09-21 11:37:55 +0300

    style

--- src/NovaPoshta.php
+++ src/NovaPoshta.php
@@ -16,11 +16,12 @@ class NovaPoshta
     public static function getCities($search = null)
     {
         $list = $search
-            ? self::get('cities.json')->filter(function ($item) use ($search) {
-                return app()->getLocale() == 'ua'
-                    ? strpos($item->Description, $search) !== false
-                    : strpos($item->DescriptionRu, $search) !== false;
-            })
+            ? self::get('cities.json')
+                ->filter(function ($item) use ($search) {
+                    return app()->getLocale() == 'ua'
+                        ? strpos($item->Description, $search) !== false
+                        : strpos($item->DescriptionRu, $search) !== false;
+                })
             : self::get('cities.json');
 
         return $list
@@ -31,11 +32,12 @@ class NovaPoshta
     public static function getWarehouses($city_id, $search = null)
     {
         $list = $search
-            ? self::get("warehouses/$city_id.json")->filter(function ($item) use ($search) {
-                return app()->getLocale() == 'ua'
-                ? strpos($item->Description, $search) !== false
-                    : strpos($item->DescriptionRu, $search) !== false;
-            })
+            ? self::get("warehouses/$city_id.json")
+                ->filter(function ($item) use ($search) {
+                    return app()->getLocale() == 'ua'
+                    ? strpos($item->Description, $search) !== false
+                        : strpos($item->DescriptionRu, $search) !== false;
+                })
             : self::get("warehouses/$city_id.json");
 
         return $list
