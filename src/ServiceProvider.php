6a6aec1e05172bc0576526d31c4c4890549073f1
scary-layer <artur.osipenko@hotmail.com>
2020-09-21 10:57:09 +0300

    feat: nova poshta command

---
+++ src/ServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ScaryLayer\NovaPoshta;
+
+use Illuminate\Support\ServiceProvider as Provider;
+
+class ServiceProvider extends Provider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([NovaPoshta::class]);
+        }
+    }
+}
